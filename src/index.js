bc4579d92989a3830066a3cacba0cb89d57f0e5d
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-20 10:58:28 +0300

    Give email a pattern and make it required

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './style.css';
+
+const form = document.querySelector('form');
+const email = document.querySelector('#mail');
+const error = email.nextElementSibling;
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
+
+window.addEventListener('load', () => {
+  const isValid = emailRegExp.test(email.value);
+  email.className = isValid || email.value.length === 0 ? 'valid' : 'invalid';
+
+  email.addEventListener('input', () => {
+    const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+    if (isValid) {
+      email.className = 'valid';
+      error.textContent = '';
+      error.className = 'error';
+    } else {
+      email.className = 'invalid';
+    }
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const isValid = email.value.length > 0 && emailRegExp.test(email.value);
+    if (!isValid) {
+      email.className = 'invalid';
+      error.textContent = 'I expect an email, darling!';
+      error.className = 'error active';
+    } else {
+      email.className = 'valid';
+      error.textContent = '';
+      error.className = 'error';
+    }
+  });
+});
